73cd74201d600d43deb6e4eff6f521727ea15812
Ugo Sans H <glinel.ugo@gmail.com>
2014-03-15 14:56:53 +0100

    Rename node configuration m6_web_log_bridge => m6web_log_bridge

--- src/M6Web/Bundle/LogBridgeBundle/DependencyInjection/Configuration.php
+++ src/M6Web/Bundle/LogBridgeBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('m6_web_log_bridge');
+        $rootNode    = $treeBuilder->root('m6web_log_bridge');
 
         $rootNode
                 ->children()
